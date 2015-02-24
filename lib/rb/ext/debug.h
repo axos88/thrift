a6aafa11e4b7d5a4891e89bb912df594a22aa02f
Devin Ben-Hur <dbenhur@whitepages.com>
2015-02-23 16:55:06 -0800

    pad in DEBUG_MSG, not DEBUG_FUNCTION_ENTRY/EXIT

--- lib/rb/ext/debug.h
+++ lib/rb/ext/debug.h
@@ -14,7 +14,7 @@ extern int debug_level;
 #define LVL_LOG 4
 #define LVL_DEBUG 5
 
-#define DEBUG_MSG(lvl, fmt, ...) ((debug_level >= lvl) ? printf("%s", separator), printf(fmt, ##__VA_ARGS__), printf("\n") : 0)
+#define DEBUG_MSG(lvl, fmt, ...) ((debug_level >= lvl) ? printf("%*s", curdepth, ''), printf(fmt, ##__VA_ARGS__), printf("\n") : 0)
 
 #define DEBUGF(fmt, ...) DEBUG_MSG(LVL_DEBUG, fmt, ##__VA_ARGS__)
 #define LOGF(fmt, ...) DEBUG_MSG(LVL_LOG, fmt, ##__VA_ARGS__)
@@ -25,8 +25,8 @@ extern int debug_level;
 
 #define DEBUGS(s) DEBUGF("%s%s\n", separator, s)
 
-#define DEBUG_FUNCTION_ENTRY() DEBUGF("%*s->%s:%s", curdepth, '', __FILE__, __FUNCTION__), curdepth += DEPTH_INCREMENT, maxdepth = curdepth > maxdepth ? curdepth : maxdepth
-#define DEBUG_FUNCTION_EXIT()  curdepth -= DEPTH_INCREMENT, DEBUGF("%*s<-%s:%s", curdepth, '', __FILE__, __FUNCTION__)
+#define DEBUG_FUNCTION_ENTRY() DEBUGF("->%s:%s", __FILE__, __FUNCTION__), curdepth += DEPTH_INCREMENT, maxdepth = curdepth > maxdepth ? curdepth : maxdepth
+#define DEBUG_FUNCTION_EXIT()  curdepth -= DEPTH_INCREMENT, DEBUGF("%s:%s", __FILE__, __FUNCTION__)
 #define DEBUG_FUNCTION_PROGRESS() DEBUGF("%s, %s:%d", __FILE__, __FUNCTION__, __LINE__)
 
 void Init_debug();
